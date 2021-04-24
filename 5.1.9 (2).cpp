01dd2b77172bbbe732d1ae256238b34178a5fec2
Singaevskyi-Max <76409361+Singaevskyi-Max@users.noreply.github.com>
2021-04-24 15:29:11 +0300

    Add files via upload

---
+++ 5.1.9 (2).cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Square
+{
+
+public:  
+
+	Square() :side(1) {};
+	
+	Square(double side);
+
+	void Set_side(double side);
+
+	void print(Square* square);
+
+private:
+	double side;
+	double area;
+	  
+};
+
+
+Square::Square(double side)
+{
+
+	this->side = side;
+	this->area = side * side;
+
+}
+
+void Square::Set_side(double side)
+{
+	if (side >= 0)
+	{
+		this->side = side;
+		this->area = side * side;
+	}
+}
+
+void Square::print(Square* square)
+{
+	cout << "Square: side=" << square->side << " area=" << square->area << endl;
+}
+
+int main()
+{
+	Square s(4);
+
+	s.print(&s);  
+
+	s.Set_side(2.0);  
+
+	s.print(&s);
+
+	s.Set_side(-33.0);
+
+	s.print(&s);  
+
+return 0;
+}
---
+++ 5.1.9 (3).cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class AdHocSquare {
+public:
+
+	AdHocSquare(double side)
+	{
+		set_side(side);
+	}
+
+	void set_side(double side)
+	{
+		this->side = side;
+	}
+
+	double get_area()
+	{
+		return side * side;
+	}
+private:
+
+	double side;
+};
+
+
+class LazySquare {
+public:
+
+	LazySquare(double side)
+	{
+		set_side(side);
+	}
+
+	void set_side(double side)
+	{
+		if (this->side != side && side >= 0) // если новое число стороны не равно тому что уже задано и не меньше нуля, то мы производим  пересчет, в ином случае не производим ни каких вычеслений 
+		{
+			this->side = side;
+
+			this->area = side * side;
+		}
+	}
+
+	double get_area()
+	{
+		return area;
+	}
+
+private:
+
+	double side;
+	double area;
+	bool side_changed; // я написал код без использования этой переменной, но решил оставить её, так как она нам давалась по условию
+};
